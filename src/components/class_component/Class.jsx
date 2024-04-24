f09d8d1f8b4a75f00135cd57d8bcd98ede80ddbb
Dario Matijak <darix135@gmail.com>
2024-04-24 20:45:00 +0200

    zadatak1

---
+++ src/components/class_component/Class.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default class Classika extends React.Component {
+  render() {
+    const lablvllvel = "nesto nesto";
+    return <p>{lablvllvel}</p>;
+  }
+}
---
+++ src/components/function_component/Function.jsx
@@ -0,0 +1,8 @@
+export default function List() {
+  return (
+    <>
+      <p>hsbdasd</p>
+      <p>dhbsadjhsa 2</p>
+    </>
+  );
+}
